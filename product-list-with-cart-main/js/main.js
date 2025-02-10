6142ea627b8bc102762f527ccf62e97fd740a94a
nurhussen muhammed <nurhussenm80@gmail.com>
2025-02-10 04:30:12 -0800

    building the product-list section

--- product-list-with-cart-main/js/main.js
+++ product-list-with-cart-main/js/main.js
@@ -5,14 +5,22 @@ async function loadProducts() {
 
     const productList = document.querySelector(".product-list");
     console.log(products);
+    const cartIcon = "../assets/images/icon-add-to-cart.svg";
 
     products.forEach((product) => {
       const productCard = `
             <div class="product-card" >
-            <img src="${product.image.thumbnail}" alt="${
+            <div class="product-image-container">
+            <img src="${product.image.desktop}" alt="${
         product.name
       }" class="product-image"/>
-            <button class="add-to-cart-btn">Add to Cart</button>
+            <button class="add-to-cart-btn">
+            <img src=${cartIcon} alt="icon" class="cart-icon">
+            Add to Cart</button>
+            </div>
+
+
+
             <div class="product-info">
             <p class="product-category">${product.category}</p>
             <h3 class="product-name">${product.name}</h3>
