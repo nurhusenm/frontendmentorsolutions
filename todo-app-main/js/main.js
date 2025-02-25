8f6836e0d467db9a549dd458d7edef784b03a6fa
nurhussen muhammed <nurhussenm80@gmail.com>
2025-02-25 12:18:06 -0800

    leyl

---
+++ todo-app-main/js/main.js
@@ -0,0 +1,8 @@
+const todoInput = document.querySelector(".todo-input");
+
+
+document.addEventListener("DOMContentLoaded", {
+
+    console.log(todoInput.value);
+})
+todoInput.addEventListener("click", () => {});
