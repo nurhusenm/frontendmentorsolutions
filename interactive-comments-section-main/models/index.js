00d497bfd1be3bec2cc602902be178250bc12441
nurhussen muhammed <nurhussenm80@gmail.com>
2025-05-08 04:36:17 -0700

    styling the comment AND REPLy section

--- interactive-comments-section-main/models/index.js
+++ interactive-comments-section-main/models/index.js
@@ -1,4 +1,5 @@
 const commentSection = document.querySelector(".comment-section");
+const replySection = document.querySelector(".reply-section");
 
 async function fetchComments() {
   try {
@@ -7,70 +8,82 @@ async function fetchComments() {
     const { comments, currentUser } = data;
 
     console.log(comments);
-    render(comments);
+    renderComments(comments); // Render main comments
 
+    // Extract all replies into a single array (if needed)
+    const allReplies = comments.reduce((acc, comment) => {
+      if (comment.replies && comment.replies.length > 0) {
+        acc.push(...comment.replies);
+      }
+      return acc;
+    }, []);
+
+    console.log(allReplies);
+    renderReplies(allReplies); // Render all replies
     return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-function createCommentsHtml(comment) {
+function createCommentHtml(comment) {
   return `
-      <div class="comments-container"> 
+        <div class="comments-container">
             <div class="comments">
                 <div class="score">
-                    <span>+</sapn>
-                    <span>${comment.score}</span>
-                    <span>-</sapn>
+                    <span class="plus"> + </span>
+                    <span class="score-number">${comment.score}</span>
+                    <span class="minus">-</span>
                 </div>
-                  <div>
-                      <div>
-                          <img src="${comment.user.image.png}" />
-                          <h3>${comment.user.username}</h3>
-                          <p>${comment.createdAt}</p>
-                          <button> &times; Reply</button>
-                      </div>
-                     <div>${comment.content}</div>
-                </div>
-                     
-             </div>
-            
-         
-          <div>
-          ${comment.replies
-            .map((reply) => {
-              return `<div>
-           <div>
-                    <span>+</sapn>
-                    <span>${reply.score}</span>
-                    <span>-</sapn>
-            </div>
                 <div>
-                <img src="${reply.user.image.png}" />
-                <h3>${reply.user.username}</h3>
-                <p>${reply.createdAt}</p>
-                <button> &times; Reply</button>
+                    <div class="header">
+                        <div class="header-left">
+                            <img src="${comment.user.image.png}" />
+                            <h4>${comment.user.username}</h4>
+                            <p>${comment.createdAt}</p>
+                        </div>
+                        <button> &#8618; Reply</button>
+                    </div>
+                    <div class="comment-text">${comment.content}</div>
+                </div>
             </div>
-                <div>${reply.content}</div>
-           </div>
-
-            </div>
-           `;
-            })
-            .join("")}
-        
-
-          
+        </div>
     `;
 }
 
-function render(comments) {
-  let allLists = "";
-  comments.map((comment) => {
-    allLists += createCommentsHtml(comment);
-  });
-  commentSection.innerHTML = allLists;
+function createReplyHtml(reply) {
+  return `
+        <div class="replies-container">
+            <div class="comments replies">
+                <div class="score">
+                    <span class="plus">+</span>
+                    <span class="score-number">${reply.score}</span>
+                    <span class="minus">-</span>
+                </div>
+                <div>
+                    <div class="header">
+                        <div class="header-left">
+                            <img src="${reply.user.image.png}" />
+                            <h3>${reply.user.username}</h3>
+                            <p>${reply.createdAt}</p>
+                        </div>
+                        <button> &#8618; Reply</button>
+                    </div>
+                    <div class="comment-text">  ${reply.content} </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function renderComments(comments) {
+  const commentsHtml = comments.map(createCommentHtml).join("");
+  commentSection.innerHTML = commentsHtml;
+}
+
+function renderReplies(replies) {
+  const repliesHtml = replies.map(createReplyHtml).join("");
+  replySection.innerHTML = repliesHtml;
 }
 
 fetchComments();
